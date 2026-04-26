53155a683e5b58944bc8b57b236814b2e3a6b45b
khliantaras <39169822+khliantaras@users.noreply.github.com>
2026-04-26 20:41:13 +0200

    Create SoldiersMap.tsx

---
+++ components/SoldiersMap.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export default function SoldiersMap() {
+    return (
+        <div style={{ width: "100%", height: "100%", background: "#050505", color: "white" }}>
+            Soldiers Map component
+        </div>
+    )
+}
